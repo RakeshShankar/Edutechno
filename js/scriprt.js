7254b11637ef57c6cef6207d73c55587ee4accbe
Rakesh S1 <rakesh.s1@mphasis.com>
2020-12-04 20:28:24 +0530

    First level Project - Front end completed

--- js/scriprt.js
+++ js/scriprt.js
@@ -1,8 +1,21 @@
-$('#view-work').on('click', function(){
-    const images = $("img").position().top;
+$(window).on("load", function () {
+    $(".loader .inner").fadeOut(300, function () {
+        $(".loader").fadeOut(500);
+    });
+})
 
-    $('html, body').animate({
-        scrollTop: images,
 
-    }, 900);
+$('#nav, .a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
 });
